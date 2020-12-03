543a78f5667f49a277535c409ab7832dd8058577
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2020-12-02 18:42:16 -0800

    PastTense.py
    An animation for another quote.

---
+++ JumpingText/PastTense.py
@@ -0,0 +1,11 @@
+# Start of script
+print ("Likes being referred to in the past tense")
+noMore = input("Press [ENTER] key to exit")
+print ("The program should now be closed. If the program is still running, try closing the window with the close button. If this doesn't work, end the task/process with a process manager/task manager")
+# Jumping text scripts are like the text on the Minecraft title menu. I may not be able to program this into a GitHub profile README but I can always make a GIF file
+""" File info
+File type: Python script file (*.py)
+Line count (including blank lines and compiler line): 12
+File version: 1 (Wednesday, December 2nd 2020 at 6:42 pm)
+"""
+# End of script
