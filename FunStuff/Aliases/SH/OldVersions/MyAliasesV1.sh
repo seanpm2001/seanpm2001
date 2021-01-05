59a572ecdf8c2b306d6840a45b58f4256cc2f442
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-01-05 15:51:49 -0800

    MyAliasesV1.sh
    An archive of the first version of my Shell Aliases file.

---
+++ FunStuff/Aliases/SH/OldVersions/MyAliasesV1.sh
@@ -0,0 +1,29 @@
+# Start of script
+# My aliases
+echo "My aliases" # Title script
+def myAliases() { # Definition of my list of aliases
+	alias spm # Define [1A] also: SPM = Sean Patrick Myrick
+	echo "spm" # Output [1B]
+	alias sww # Repeat step 1A also: SWW = Sean Walla Walla
+	echo "sww" # Repeat step 1B
+	alias seanpm # Paper signature
+	echo "seanpm" # Paper signature
+	alias seanpm2001 # See below
+	echo "seanpm2001" # GitHub username alias, started May 25th 2020
+	alias seantrm sup-d-o -r-m -rtfm -no-presorve-rouuoot- # Deformed, so you don't make the mistake and mess up your system, this also signals that I no longer use this alias
+	echo "seantrm (no longer in use as of August 2018)" # See above for less info
+	alias slm # Not to be confused with slim
+	echo "slm" # Sean Language Myrick
+	alias seanlm # Stands for: sean Language Myrick
+	echo "seanlm" # See above
+	alias seanlm2001 # Stands for: Sean Language Myrick 2001
+	echo "seanlm2001" # See above
+} # End of definition list function
+myAliases() # Executes the definition list
+echo "Alias commands currently don't work" # See 23
+break; # I don't know what this does in Shell, I think it does the same thing done in Java, but I don't know. It is currently just a vanity element
+# File info
+# File version: 1 (Tuesday, January 5th 2021 at 3:51 pm)
+# File type: Shell script file (*.sh)
+# Line count (including blank lines and compiler line): 30
+# End of script
