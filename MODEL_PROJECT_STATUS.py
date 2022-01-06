b75f5e22f1c4430806c4335a3b576d631da21cfe
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2022-01-05 22:06:06 -0800

    MODEL_PROJECT_STATUS.py (V1)
    The current version (version 1) of the file explaining this project as a model project.

---
+++ MODEL_PROJECT_STATUS.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Start of script
+"""
+MODEL_PROJECT_STATUS.py
+Information about this projects use as a model project
+"""
+
+def modelProjectStatus():
+  print ("Model project status: This is a model project")
+  print ("This is one of @seanpm2001's Model projects. It is certified as a project ready for use as a first impression into seanpm2001 and his works")
+  sleep(1)
+  break
+return modelProjectStatus()
+# print ("Model project status: This is a model project")
+# print ("This is one of @seanpm2001's Model projects. It is certified as a project ready for use as a first impression into seanpm2001 and his works")
+# sleep(1)
+noMore = input("Press [ENTER] key to exit")
+print("The program should now be closed. If you are not using Python in a terminal, and the program is still open, try manually closing it with the close button. If that doesn't work, try ending the process with a process/task manager.")
+break
+
+'''
+File info
+File type: Python source file (*.py)
+File version: 1 (2022, Wednesday, January 5th at 10:06 pm)
+Line count (including blank lines and compiler line: 28
+'''
+# End of script
